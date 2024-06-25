fd0444fc7fb91d766d1125a54d8131e03b31b92a
dmorr041 <quinncodes92@gmail.com>
2024-06-24 20:22:40 -0500

    init

---
+++ src/main/java/com/algorhythmic92/ws/benchmark/WsBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package com.algorhythmic92.ws.benchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBenchmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorhythmic92/ws/benchmark/WsBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorhythmic92.ws.benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
