078bee82f2cca07533decd96c094e381889ebc2f
dmorr041 <quinncodes92@gmail.com>
2024-06-25 09:00:12 -0500

    Revert "Created empty ExerciseRepository implementing Spring JPA CrudRepository. Converted Exercise.java to a JPA Entity"
    This reverts commit 276515bb71e7a3ef952efa6929bb3e7c280f9825.

--- src/main/java/com/algorhythmic92/ws/benchmark/dto/Exercise.java
+++ src/main/java/com/algorhythmic92/ws/benchmark/dto/Exercise.java
@@ -1,24 +1,19 @@
 package com.algorhythmic92.ws.benchmark.dto;
 
 import com.algorhythmic92.ws.benchmark.enumeration.ExerciseVariation;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class Exercise {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
     private Long id;
-
     private String name;
     private ExerciseVariation variation;
     private Integer weight;
